d2ee28167a3fb542baf72b882bade8c3021bf24d
simonmartyr <simonmartyr@hotmail.co.uk>
2020-10-06 23:59:48 +0200

    data store

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
+<<<<<<< HEAD
 ### Good evening 🌙
+=======
+## Good evening 🌙
+>>>>>>> data store
 
 I'm Simon Martyr 🇬🇧, an Amsterdam 🇳🇱 based developer. 
 
 Working for [@Finaps](https://www.finaps.nl/) 
 
 
+<<<<<<< HEAD
 ### Social Stuff 🐯
+=======
+## Social Stuff 👆
+>>>>>>> data store
 
 [![Twitter Badge](https://img.shields.io/badge/-@vintage_si-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/vintage_si)](https://twitter.com/vintage_si)
 
-### Stats 🤖
+## Stats 🤖
 
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=simonmartyr&layout=compact)](https://github.com/anuraghazra/github-readme-stats)
 
+<<<<<<< HEAD
 ### Last Caught Pokemon: Non-Shiny - silcoon
  
 ![pokemon](https:&#x2F;&#x2F;raw.githubusercontent.com&#x2F;PokeAPI&#x2F;sprites&#x2F;master&#x2F;sprites&#x2F;pokemon&#x2F;266.png) 
+=======
+## Last Caught Pokemon: Non-Shiny - shieldon
+ 
+![pokemon](https:&#x2F;&#x2F;raw.githubusercontent.com&#x2F;PokeAPI&#x2F;sprites&#x2F;master&#x2F;sprites&#x2F;pokemon&#x2F;410.png) 
+
+### Most Caught
+
+Pokemon | Quanity |
+--- | --- |
+squirtle|4
+bob|1
+uxie|1
+anorith|1
+gorebyss|1
+shieldon|1
+
+>>>>>>> data store
 
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fetch = require("node-fetch");
 const randomEmoji = require("random-emoji");
 
 const creationDate = new Date("5 Oct, 2020 22:00");
+let pokemon;
 
 /**
  * DATA is the object that contains all
@@ -16,12 +17,9 @@ let DATA = {
   name: "Simon",
   welcome: welcomeText(),
   emoji: randomEmoji.random()[0].character,
+  caughtList: "",
 };
-/**
- * A - We open 'main.mustache'
- * B - We ask Mustache to render our file with the data
- * C - We create a README.md file with the generated output
- */
+
 function welcomeText() {
   let amsterdamHours = new Date().getUTCHours() + 2;
   if (amsterdamHours < 12) {
@@ -45,19 +43,44 @@ async function captureAPokemon() {
   var pokemonToGet = Math.floor(Math.random() * 807);
   var isShiny = Math.floor(Math.random() * 8193) == 8192;
 
-  await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonToGet}`)
-    .then((r) => r.json())
-    .then((r) => {
-      (DATA.pokemonImage = isShiny
-        ? r.sprites.front_shiny
-        : r.sprites.front_default),
-        (DATA.pokemonName = r.name),
-        (DATA.isShiny = isShiny ? "Shiny" : "Non-Shiny");
-    });
+  const {
+    sprites: { front_default, front_shiny },
+    name,
+  } = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonToGet}`
+  ).then((r) => r.json());
+
+  DATA.pokemonImage = isShiny ? front_shiny : front_default;
+  DATA.pokemonName = name;
+  DATA.isShiny = isShiny ? "Shiny" : "Non-Shiny";
+
+  var previouslyCaught = pokemon.caught.find((x) => x.name == name);
+  previouslyCaught
+    ? previouslyCaught.qt++
+    : pokemon.caught.push({ name, qt: 1 });
+  pokemon.caught.sort((a, b) => b.qt - a.qt);
+  top10 = pokemon.caught.slice(0, 9);
+  top10.forEach((element) => {
+    DATA.caughtList += `${element.name}|${element.qt}\n`;
+  });
+}
+
+function readPokemon() {
+  fs.readFile("pokedex.json", (err, data) => {
+    if (err) throw err;
+    pokemon = JSON.parse(data);
+  });
+}
+
+function writePokemon() {
+  let data = JSON.stringify(pokemon);
+  fs.writeFileSync("pokedex.json", data);
 }
 
 async function action() {
+  readPokemon();
   await captureAPokemon();
+  writePokemon();
 
   await generateReadMe();
 }
