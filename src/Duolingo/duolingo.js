15c26276107bbb5ed32b7d62d56f5a13d36254ea
simonmartyr <simonmartyr@hotmail.co.uk>
2020-10-29 02:25:18 +0100

    reverted duolingo file

--- src/Duolingo/duolingo.js
+++ src/Duolingo/duolingo.js
@@ -1,6 +1,6 @@
 const Duolingo = require("duolingo-api");
 const credentials = {
-  id: "32246515",
+  id: process.env.DUOLINGO_ID,
 };
 let duolingo = new Duolingo(credentials);
 
@@ -8,9 +8,5 @@ async function getDuolingoXp() {
   const myFields = ["totalXp"];
   return await duolingo.getDataByFields(myFields);
 }
-async function test() {
-  var l = await getDuolingoXp();
-  console.log(l);
-}
-test();
+
 module.exports.getDuolingoXp = getDuolingoXp;
